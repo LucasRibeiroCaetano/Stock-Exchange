34ebb7a64c128f76661246fb6120e0c363dfc608
Lucas Caetano <lucas.rib.caetano@gmail.com>
2024-03-19 15:41:13 +0000

    update

--- bolsa/bolsa.c
+++ bolsa/bolsa.c
@@ -1,21 +1,20 @@
 #define UNICODE
 
 #include <stdio.h>
-#include <string.h>
-#include <windows.h>
 #include <tchar.h>
+#include <windows.h>
 #include <fcntl.h>
 #include <io.h>
-#include <stdio.h>
 
 #include "data.h"
+
+#define STR_LEN 100 // Assuming maximum length of strings
 
 int _tmain(int argc, TCHAR* argv[]) {
 
     TCHAR fileName[STR_LEN];
     Utilizador utilizadores[MAX_USERS];
     DWORD numUtilizadores = 0;
-
 
 #ifdef UNICODE 
     _setmode(_fileno(stdin), _O_WTEXT);
@@ -28,48 +27,43 @@ int _tmain(int argc, TCHAR* argv[]) {
     }
 
     _tprintf_s(_T("=|| Bolsa ||=\n"));
-    _tprintf_s(_T("A ler do ficheiro de texto..."));
+    _tprintf_s(_T("A ler do ficheiro de texto...\n"));
 
-    // Copia o nome do ficheiro dos argumentos
     if (_tcscpy_s(fileName, STR_LEN, argv[1]) != 0) {
         Abort(_T("Erro ao copiar o nome do arquivo.\n"));
     }
 
-    // Abrir o ficheiro
+    _tprintf_s(_T("\n\nNome do ficheiro: %s\n"), fileName);
+
     FILE* file;
     if (_tfopen_s(&file, fileName, _T("r")) != 0 || file == NULL) {
         Abort(_T("Falha ao abrir o arquivo de utilizadores.\n"));
     }
 
-    // Ler o conteúdo do ficheiro e armazenar nos utilizadores
-    while (numUtilizadores < MAX_USERS) {
+    while (numUtilizadores < MAX_USERS && !feof(file)) {
+        if (_fgetts(utilizadores[numUtilizadores].username, STR_LEN, file) == NULL)
+            Abort("Erro ao ler os usernames dos utilizadores.");
 
-        // Username
-        if (_fgetts(utilizadores[numUtilizadores].username, STR_LEN, fileName) == NULL)
-            Abort("Ficheiro de utilizadores -> Utilizador Inválido");
+        if (_fgetts(utilizadores[numUtilizadores].password, STR_LEN, file) == NULL)
+            Abort("Erro ao ler as passwords dos utilizadores.");
 
-        // Password
-        if (_fgetts(utilizadores[numUtilizadores].password, STR_LEN, fileName) == NULL)
-            Abort("Ficheiro de utilizadores -> Password Inválido");
-
-        // Saldo
-        if (_ftscanf_s(fileName, _T("%f"), &utilizadores[numUtilizadores].saldo) != 1)
-            Abort("Ficheiro de utilizadores -> Saldo Inválido");
+        if (_ftscanf_s(file, _T("%f"), &utilizadores[numUtilizadores].saldo) != 1)
+            Abort("Erro ao ler o saldo dos utilizadores.");
 
         numUtilizadores++;
     }
 
+
     fclose(file);
 
-    // Mostrar os utilizadores
+    _tprintf_s(_T("\nChegou aqui. numUtil: %u\n"), numUtilizadores);
+
     for (DWORD i = 0; i < numUtilizadores; i++) {
         _tprintf(_T("Utilizador %d:\n"), i + 1);
         _tprintf(_T("Username: %s\n"), utilizadores[i].username);
         _tprintf(_T("Password: %s\n"), utilizadores[i].password);
         _tprintf(_T("Saldo: %.2f\n"), utilizadores[i].saldo);
     }
-
-    // Entrar na registry ver se existe um valor NCLIENTES, se não existir NCLIENTES = 5 na registry
 
     return 0;
 }
--- bolsa/utils.h
+++ bolsa/utils.h
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <tchar.h>
-
+#include <windows.h>
 
 
 void Abort(const TCHAR* message);
